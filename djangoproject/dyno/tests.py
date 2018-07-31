dd5bd60ea3b025899fcbf82dd78e3d4d34eecb90
Wrodgers69 <wrodg219@gmail.com>
2018-07-30 23:31:03 -0500

    added client tests to verify login requirement functions for each view/form. Also queries for views that utilize Metabase. confirms foreign key is working correctly

--- djangoproject/dyno/tests.py
+++ djangoproject/dyno/tests.py
@@ -4,6 +4,26 @@ from django.test import TestCase
 import datetime
 from django.utils import timezone
 from .models import Card_Info, Well_Profile, Dysfunction_Profile
+from django.test import Client
+from django.contrib.auth.models import User
+import unittest
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse, reverse_lazy
+
+
+#class ModelReturnTests(TestCase):
+
+#    def create_well_profile(self, well_name="test well"):
+#        return Well_Profile.objects.create(well_name=well_name)
+
+#    def create_card_info(self, title="only a test", associated_well_profile="test well"):
+#        return Card_Info.objects.create(title=title, associated_well_profile=associated_well_profile)
+
+#    def test_card_info_Creation(self):
+#        w = self.create_card_info()
+#        self.assertTrue(isinstance(w, Card_Info))
+#        self.assertEqual(w.__unicode__(), w.title)
+
 
 class ModelTests(TestCase):
 
@@ -98,3 +118,211 @@ class ModelTests(TestCase):
 #        new = Well_Profile(well_name="test 1h", date_created=previously, date_updated=now)
 #        new.save()
 #        self.assertIs(new.save(), True)
+
+class Test_Well_Information(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(self):
+        #creating client instance
+        self.client = Client()
+        self.user = User.objects.create_user('wade','wade@wade.com','p@ssword')
+
+    def test_well_information_view_login(self):
+        self.client.login(username='wade', password='p@ssword')
+        response = self.client.get(reverse_lazy('dyno:well_information'))
+        #check for reponse is 200 okself.
+        self.assertEqual(response.status_code, 200)
+
+class Test_Home_View(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(self):
+        self.client = Client()
+        self.user = User.objects.create_user('lee', 'wade@wade.com', 'p@ssword')
+
+    def test_home_view_login(self):
+        self.client.login(username='lee', password='p@ssword')
+        response = self.client.get(reverse_lazy('dyno:home'))
+        self.assertEqual(response.status_code, 200)
+
+class Test_Home_View_Fails(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(self):
+        self.client = Client()
+        #self.user = User.objects.create_user('lee', 'wade@wade.com', 'p@ssword')
+
+    def test_home_view_login(self):
+        #self.client.login(username='lee', password='p@ssword')
+        response = self.client.get(reverse_lazy('dyno:home'))
+        self.assertEqual(response.status_code, 302)
+
+class Test_Well_Information_View_Fails(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(self):
+        self.client = Client()
+        #self.user = User.objects.create_user('lee', 'wade@wade.com', 'p@ssword')
+
+    def test_home_view_login(self):
+        #self.client.login(username='lee', password='p@ssword')
+        response = self.client.get(reverse_lazy('dyno:well_information'))
+        self.assertEqual(response.status_code, 302)
+
+class Test_Success_View(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(self):
+        self.client = Client()
+        self.user = User.objects.create_user('james', 'wade@wade.com', 'p@ssword')
+
+    def test_home_view_login(self):
+        self.client.login(username='james', password='p@ssword')
+        response = self.client.get(reverse_lazy('dyno:success'))
+        self.assertEqual(response.status_code, 200)
+
+class Test_Success_View_Fails(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(self):
+        self.client = Client()
+        #self.user = User.objects.create_user('lee', 'wade@wade.com', 'p@ssword')
+
+    def test_home_view_login(self):
+        #self.client.login(username='lee', password='p@ssword')
+        response = self.client.get(reverse_lazy('dyno:success'))
+        self.assertEqual(response.status_code, 302)
+
+class Test_Diagnose_View(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(self):
+        self.client = Client()
+        self.user = User.objects.create_user('wade2', 'wade@wade.com', 'p@ssword')
+
+    def test_home_view_login(self):
+        self.client.login(username='wade2', password='p@ssword')
+        response = self.client.get(reverse_lazy('dyno:diagnose'))
+        self.assertEqual(response.status_code, 200)
+
+class Test_Diagnose_View_Fails(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(self):
+        self.client = Client()
+        #self.user = User.objects.create_user('lee', 'wade@wade.com', 'p@ssword')
+
+    def test_home_view_login(self):
+        #self.client.login(username='lee', password='p@ssword')
+        response = self.client.get(reverse_lazy('dyno:diagnose'))
+        self.assertEqual(response.status_code, 302)
+
+class Test_PredictResults_View(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(self):
+        self.client = Client()
+        self.user = User.objects.create_user('wade3', 'wade@wade.com', 'p@ssword')
+        well = Well_Profile.objects.create(well_name='test well 1')
+        card = Card_Info.objects.create(associated_well_profile=well, title='test card', img_file='test image', prediction='good')
+
+    def test_home_view_login(self):
+        self.client.login(username='wade3', password='p@ssword')
+        response = self.client.get(reverse_lazy('dyno:predict_results'))
+        self.assertEqual(response.status_code, 200)
+
+class Test_PredictResults_View_Fails(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(self):
+        self.client = Client()
+        #self.user = User.objects.create_user('lee', 'wade@wade.com', 'p@ssword')
+
+    def test_home_view_login(self):
+        #self.client.login(username='lee', password='p@ssword')
+        response = self.client.get(reverse_lazy('dyno:predict_results'))
+        self.assertEqual(response.status_code, 302)
+
+
+class Test_Dashboard_View(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(self):
+        self.client = Client()
+        self.user = User.objects.create_user('wade4', 'wade@wade.com', 'p@ssword')
+        well = Well_Profile.objects.create(well_name='test well 1')
+        card = Card_Info.objects.create(associated_well_profile=well, title='test card', img_file='test image', prediction='good')
+
+    def test_home_view_login(self):
+        self.client.login(username='wade4', password='p@ssword')
+        response = self.client.get(reverse_lazy('dyno:dashboard'))
+        self.assertEqual(response.status_code, 200)
+
+class Test_Dashboard_View_Fails(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(self):
+        self.client = Client()
+        #self.user = User.objects.create_user('lee', 'wade@wade.com', 'p@ssword')
+
+    def test_home_view_login(self):
+        #self.client.login(username='lee', password='p@ssword')
+        response = self.client.get(reverse_lazy('dyno:dashboard'))
+        self.assertEqual(response.status_code, 302)
+
+
+class Test_CommandCenter_View(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(self):
+        self.client = Client()
+        self.user = User.objects.create_user('wade5', 'wade@wade.com', 'p@ssword')
+        well = Well_Profile.objects.create(well_name='test well 1')
+        card = Card_Info.objects.create(associated_well_profile=well, title='test card', img_file='test image', prediction='good')
+
+    def test_home_view_login(self):
+        self.client.login(username='wade5', password='p@ssword')
+        response = self.client.get(reverse_lazy('dyno:command_center'))
+        self.assertEqual(response.status_code, 200)
+
+class Test_CommandCenter_View_Fails(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(self):
+        self.client = Client()
+        #self.user = User.objects.create_user('lee', 'wade@wade.com', 'p@ssword')
+
+    def test_home_view_login(self):
+        #self.client.login(username='lee', password='p@ssword')
+        response = self.client.get(reverse_lazy('dyno:command_center'))
+        self.assertEqual(response.status_code, 302)
+
+#class Test_Checkpoint_View(unittest.TestCase):
+
+#    @classmethod
+#    def setUpClass(self):
+#        self.client = Client()
+#        self.user = User.objects.create_user('wade6', 'wade@wade.com', 'p@ssword')
+
+#    def test_home_view_login(self):
+#        self.client.login(username='wade6', password='p@ssword')
+#        response = self.client.get(reverse_lazy('dyno:checkpoint'))
+#        self.assertEqual(response.status_code, 200)
+
+class Test_Checkpoint_View_Fails(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(self):
+        self.client = Client()
+        #self.user = User.objects.create_user('lee', 'wade@wade.com', 'p@ssword')
+
+    def test_home_view_login(self):
+        #self.client.login(username='lee', password='p@ssword')
+        response = self.client.get(reverse_lazy('dyno:checkpoint'))
+        self.assertEqual(response.status_code, 302)
+
+#class TestMustLogin(unittest.TestCase):
+
+#    @classmethod
+#    def setUpClass(self):
+        #self.
